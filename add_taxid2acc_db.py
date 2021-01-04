12ff7075b66df9d734aecb572c06f4f8c20d9cbc
frederikseersholm <frederikseersholm@gmail.com>
2021-01-04 11:29:26 +0100

    Add files via upload

--- add_taxid2acc_db.py
+++ add_taxid2acc_db.py
@@ -7,7 +7,7 @@ from Bio import SeqIO
 
 import glob
 def main():
-   
+
     p = optparse.OptionParser()
     p.add_option('--outfile', '-o')
     options, arguments = p.parse_args()
@@ -22,76 +22,73 @@ def main():
     gi_list=[]
     for record in SeqIO.parse(fastafile,format='fasta'):
 	#print record
-	if '|' in record.description:
-        	split_header=record.description.split('|')
-        	#print split_header
-		gi_nr=[split_header[i+1] for i,j in enumerate(split_header) if j=='ref'][0]
-        	if gi_nr not in gi_list:
-            		gi_list.append(gi_nr)
-	elif 'NC' in record.description:
-		#print record.description
-		split_header=record.description.split(' ')
-                gi_nr=[i for i in split_header if 'NC' in i][0]
-                if gi_nr not in gi_list:
-                        gi_list.append(gi_nr)
-	else:
-		continue
+        if '|' in record.description:
+            split_header=record.description.split('|')
+            #print split_header
+            gi_nr=[split_header[i+1] for i,j in enumerate(split_header) if j=='ref'][0]
+            if gi_nr not in gi_list:
+                gi_list.append(gi_nr)
+        elif 'NC' in record.description:
+            #print record.description
+            split_header=record.description.split(' ')
+            gi_nr=[i for i in split_header if 'NC' in i][0]
+            if gi_nr not in gi_list:
+                gi_list.append(gi_nr)
+            else:
+                continue
 ###################
     gi_dict={}
     for gi_nr in gi_list:
         gi_dict[gi_nr]='TAXID_NOT_FOUND'
 ####################
     count=0
-    filelist=glob.glob('/PATH/to/FILE/Accession2taxid/nucl_*.accession2taxid.??')	
+    filelist=glob.glob('/willerslev/users-shared/science-snm-willerslev-cqr376/software/getLCA/DBfiles_getLCA/Accession2taxid/nucl_*.accession2taxid.??')
     for gi_name in filelist:
         count+=1
         #if count>2:
         #    break
-        print '\nprocessing file '+str(count)+' of '+str(len(filelist))+'\n'
+        print('\nprocessing file '+str(count)+' of '+str(len(filelist))+'\n')
         gi=open(gi_name,'r')
         gi2taxid={}
-        
+
         lines_processed = 0
         for line in gi.readlines():
             lines_processed = lines_processed + 1
             if (lines_processed % 5000000 == 0):
                 sys.stdout.write('-')
-            
+
             text=line.split()
             gi2taxid[text[1]]=text[2]
-        
+
         gi.close()
-        
+
         for gi_nr in gi_list:
             if gi_dict[gi_nr]=='TAXID_NOT_FOUND':
                 try:
                     gi_dict[gi_nr]=gi2taxid[gi_nr]
-                
+
                 except:
                     gi_dict[gi_nr]='TAXID_NOT_FOUND'
-        
-    print '\n"gi_taxid_nucl.dmp"-files loaded into memory \n'    
+
+    print('\n"gi_taxid_nucl.dmp"-files loaded into memory \n')
     count=0
     outfile=open(outfile,'w')
     for record in SeqIO.parse(fastafile,format='fasta'):
         #count+=1
         #if count>100:
         #    break
-        
-	if '|' in record.description:	
-    	    split_header=record.description.split('|')
-      	    gi=[split_header[i+1] for i,j in enumerate(split_header) if j=='ref'][0]
+        if '|' in record.description:
+            split_header=record.description.split('|')
+            gi=[split_header[i+1] for i,j in enumerate(split_header) if j=='ref'][0]
 
-    	elif 'NC' in record.description:
-	    split_header=record.description.split(' ')
+        elif 'NC' in record.description:
+            split_header=record.description.split(' ')
             gi=[i for i in split_header if 'NC' in i][0]
-    	else:
-	    gi='not_found'
+        else:
+            gi='not_found'
 
-    	taxid=gi_dict.get(gi,'TAXID_NOT_FOUND_in_dict')            
-    	record.description=taxid+'|'+record.description
-        
-        
+        taxid=gi_dict.get(gi,'TAXID_NOT_FOUND_in_dict')
+        record.description=taxid+'|'+record.description
         outfile.write('>'+str(record.description)+'\n'+str(record.seq)+'\n')
     outfile.close()
 
